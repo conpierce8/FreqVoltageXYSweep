8bf032049fc8c1735036d38d1b11c252afbc28c4
Connor D. Pierce <conpierce8@gmail.com>
2022-09-15 15:11:06 -0500

    Added software license to more files

--- python/live_lockin_data.py
+++ python/live_lockin_data.py
@@ -1,9 +1,54 @@
+#! python3
+# -*- coding: utf-8 -*-
+#
+# live_lockin_data.py
+#
+# Plot data acquired by lockin amplifier in real-time.
+#
+# Author:   Connor D. Pierce
+# Created:  2018-06-29 13:35
+# Modified: 2022-09-15 15:10:50
+#
+# Copyright (c) 2018-2022 Connor D. Pierce
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# SPDX-License-Identifier: MIT
+
+
+"""Plot lock-in data in real-time.
+
+Usage:
+
+```
+python live_lockin_data.py <filename>
+```
+
+where `filename` is the path to a file where FreqVoltageXYSweep is currently
+saving data.
+"""
+
 import numpy as np
 from matplotlib import pyplot
 import matplotlib.animation as animation
 import sys
 
-#20180629/midstiffness/long time const/outputSpacer_01.txt
 
 freq = np.array([])
 data = np.array([])
