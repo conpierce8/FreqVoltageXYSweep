bd25a050b11527ed5b9f0b90c4a77cf35225be54
Connor D. Pierce <conpierce8@gmail.com>
2022-09-15 15:07:14 -0500

    Added software licenses to src files

--- src/LockinSettings.cpp
+++ src/LockinSettings.cpp
@@ -1,3 +1,35 @@
+// LockinSettings.cpp
+// encoding: utf-8
+//
+// Manage lock-in amplifier settings.
+//
+// Author:   Connor D. Pierce
+// Created:  2018-07-02 15:48
+// Modified: 2022-09-15 15:04:16
+//
+// Copyright (C) 2018-2022 Connor D. Pierce
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// SPDX-License-Identifier: MIT
+
+
 #ifndef LOCKINSETTINGS_CPP_
 #define LOCKINSETTINGS_CPP_
 
--- src/resource.h
+++ src/resource.h
@@ -1,9 +1,34 @@
-/*
- * resource.h
- *
- *  Created on: Feb 1, 2018
- *      Author: Connor D. Pierce
- */
+// resource.h
+// encoding: utf-8
+//
+// FreqVoltageXYSweep Win32 resource header file.
+//
+// Author:   Connor D. Pierce
+// Created:  2018-02-01
+// Modified: 2022-09-15 14:44:42
+//
+// Copyright (C) 2022 Connor D. Pierce
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// SPDX-License-Identifier: MIT
+
 
 #ifndef RESOURCE_H_
 #define RESOURCE_H_
